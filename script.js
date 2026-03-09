7fd29b86345af95e9d7ce38c8cc3500c758cca36
PyroNaru <relampagosmiguel438@gmail.com>
2026-03-09 13:38:56 +0800

    feat: add simple admin dashboard and manage users page

--- script.js
+++ script.js
@@ -19,7 +19,13 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
       }
 
-      window.location.href = "profile.html";
+      if (username === "admin") {
+        localStorage.setItem("userRole", "admin");
+        window.location.href = "admin.html";
+      } else {
+        localStorage.setItem("userRole", "user");
+        window.location.href = "profile.html";
+      }
     });
   }
 
@@ -57,5 +63,59 @@ document.addEventListener("DOMContentLoaded", function () {
   function validateEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
+  }
+});
+
+function deleteRow(button) {
+  const row = button.parentNode.parentNode;
+  row.remove();
+}
+
+const addUserForm = document.getElementById("addUserForm");
+
+if (addUserForm) {
+  addUserForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("newName").value.trim();
+    const email = document.getElementById("newEmail").value.trim();
+    const role = document.getElementById("newRole").value;
+
+    // To get existing users
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    // Add new user
+    users.push({
+      name: name,
+      email: email,
+      role: role,
+    });
+
+    // Save back to localStorage
+    localStorage.setItem("users", JSON.stringify(users));
+
+    alert("User Added: " + name);
+
+    addUserForm.reset();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const table = document.getElementById("userTable");
+
+  if (table) {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    users.forEach((user) => {
+      const row = table.insertRow();
+
+      row.insertCell(0).textContent = user.name;
+      row.insertCell(1).textContent = user.email;
+      row.insertCell(2).textContent = user.role;
+
+      const actionCell = row.insertCell(3);
+      actionCell.innerHTML =
+        '<button onclick="deleteRow(this)">Delete</button>';
+    });
   }
 });
